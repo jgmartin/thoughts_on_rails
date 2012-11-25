bb9a17f09242cba4c818c56bc8710b5135e802e5
James Martin <jmartin2683@gmail.com>
2012-11-25 13:15:06 -0500

    added bing verification meta tag

---
+++ plugins/s3_image_tag.rb
@@ -0,0 +1,31 @@
+module Jekyll
+  class S3ImageTag < Liquid::Tag
+
+    def initialize(tag_name, text, token)
+      super
+      @text = text.strip
+      @text =~ /(\w+\.\w+)(\sbucket:)?(\w*)/i
+      @file_name = $1
+      @bucket_name = $3.empty? ? get_config('aws_bucket') : $3
+    end
+
+    def render(context)
+      output = super
+      if @file_name && @bucket_name
+        image_tag = "<img src='https://s3.amazonaws.com/#{@bucket_name}/#{@file_name}'>"
+      else
+        "Error processing input, expected syntax: {% s3_image file_name [bucket:bucket_name] %}"
+      end
+    end
+
+
+    private
+
+    def get_config(string)
+      Jekyll.configuration({})[string]
+    end
+  end
+
+end
+
+Liquid::Template.register_tag('s3_image', Jekyll::S3ImageTag)
